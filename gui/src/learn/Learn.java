177e20c2d385923769e9aa4b09d39a4d157b2d07
cmadsen <>
2007-02-22 23:06:28 +0000

    Update for Learn

--- gui/src/learn/Learn.java
+++ gui/src/learn/Learn.java
@@ -4,9 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
-
 import javax.swing.*;
-
 import buttons.core.gui.*;
 
 /**
@@ -36,6 +34,8 @@ public class Learn implements ActionListener {
 
 	private JTextField activation, repression, parent;
 
+	private JTextField background;
+
 	private JTextField windowRising, windowSize, numBins;
 
 	private JTextField influenceLevel, relaxIPDelta, letNThrough;
@@ -45,13 +45,15 @@ public class Learn implements ActionListener {
 	private JCheckBox mustNotWinMajority, donotTossSingleRatioParents,
 			donotTossChangedInfluenceSingleParents;
 
-	private ArrayList<ArrayList<JTextField>> species;
+	private ArrayList<ArrayList<Component>> species;
 
-	private ArrayList<JComboBox> numberOfLevels;
+	// private ArrayList<JComboBox> numberOfLevels;
 
 	private JPanel speciesPanel;
 
 	private JRadioButton user, auto;
+
+	private JButton suggest;
 
 	/**
 	 * This is the constructor for the Learn class. It initializes all the input
@@ -93,14 +95,17 @@ public class Learn implements ActionListener {
 		JPanel selection = new JPanel();
 		user = new JRadioButton("Use User Generated Levels");
 		auto = new JRadioButton("Use Auto Generated Levels");
+		suggest = new JButton("Suggest Levels");
 		ButtonGroup select = new ButtonGroup();
-		select.add(user);
 		select.add(auto);
-		user.setSelected(true);
+		select.add(user);
+		auto.setSelected(true);
 		user.addActionListener(this);
 		auto.addActionListener(this);
-		selection.add(user);
+		suggest.addActionListener(this);
 		selection.add(auto);
+		selection.add(user);
+		selection.add(suggest);
 		JPanel encodingPanel = new JPanel(new BorderLayout());
 		speciesPanel = new JPanel();
 		JLabel encodingsLabel = new JLabel("Species Levels:");
@@ -126,7 +131,6 @@ public class Learn implements ActionListener {
 		initNetwork = new JTextField(39);
 		experiments = new JTextField(39);
 		experiments.setText(directory);
-		levels();
 		initNet1.add(initNetLabel);
 		initNet1.add(initNetwork);
 		initNet1.add(browseInit);
@@ -137,7 +141,7 @@ public class Learn implements ActionListener {
 		initNet.add(initNet2);
 
 		// Sets up the thresholds area
-		JPanel thresholdPanel = new JPanel(new GridLayout(13, 2));
+		JPanel thresholdPanel = new JPanel(new GridLayout(14, 2));
 		JLabel debugLabel = new JLabel("Debug Level:");
 		String[] options = new String[4];
 		options[0] = "0";
@@ -179,11 +183,14 @@ public class Learn implements ActionListener {
 		relaxIPDelta = new JTextField("0.025");
 		thresholdPanel.add(relaxIPDeltaLabel);
 		thresholdPanel.add(relaxIPDelta);
-		JLabel letNThroughLabel = new JLabel(
-				"Minimum Number Of SelectInitalParents:");
+		JLabel letNThroughLabel = new JLabel("Minimum Number Of SelectInitalParents:");
 		letNThrough = new JTextField("1");
 		thresholdPanel.add(letNThroughLabel);
 		thresholdPanel.add(letNThrough);
+		JLabel backgroundLabel = new JLabel("Background Knowledge Filter:");
+		background = new JTextField("0.0");
+		thresholdPanel.add(backgroundLabel);
+		thresholdPanel.add(background);
 		harshenBoundsOnTie = new JCheckBox("Harshen Bounds On Tie");
 		harshenBoundsOnTie.setSelected(true);
 		donotInvertSortOrder = new JCheckBox("Do Not Invert Sort Order");
@@ -192,8 +199,7 @@ public class Learn implements ActionListener {
 		seedParents.setSelected(true);
 		mustNotWinMajority = new JCheckBox("Must Not Win Majority");
 		mustNotWinMajority.setSelected(true);
-		donotTossSingleRatioParents = new JCheckBox(
-				"Single Ratio Parents Should Be Kept");
+		donotTossSingleRatioParents = new JCheckBox("Single Ratio Parents Should Be Kept");
 		donotTossChangedInfluenceSingleParents = new JCheckBox(
 				"Parents That Change Influence Should Not Be Tossed");
 		thresholdPanel.add(harshenBoundsOnTie);
@@ -216,8 +222,7 @@ public class Learn implements ActionListener {
 		file.setMnemonic(KeyEvent.VK_F);
 		menuBar.add(file);
 		close = new JMenuItem("Close");
-		close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,
-				ActionEvent.ALT_MASK));
+		close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.ALT_MASK));
 		close.setMnemonic(KeyEvent.VK_C);
 		close.addActionListener(this);
 		file.add(close);
@@ -225,11 +230,13 @@ public class Learn implements ActionListener {
 		// Creates the main panel
 		JPanel mainPanel = new JPanel(new BorderLayout());
 		JPanel middlePanel = new JPanel(new BorderLayout());
-		middlePanel.add(thresholdPanel, "North");
-		middlePanel.add(radioPanel, "South");
+		middlePanel.add(radioPanel, "Center");
+		middlePanel.add(runHolder, "South");
 		mainPanel.add(initNet, "North");
-		mainPanel.add(middlePanel, "Center");
-		mainPanel.add(runHolder, "South");
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, middlePanel, null);
+		splitPane.setDividerSize(0);
+		mainPanel.add(thresholdPanel, "Center");
+		mainPanel.add(splitPane, "South");
 
 		// Packs the frame and displays it
 		frame.setContentPane(mainPanel);
@@ -261,45 +268,22 @@ public class Learn implements ActionListener {
 	 * buttons are selected.
 	 */
 	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().contains("box")) {
-			String s = e.getActionCommand().substring(0,
-					e.getActionCommand().length() - 3);
-			for (int i = 0; i < species.size(); i++) {
-				if (species.get(i).get(0).getText().equals(s)) {
-					if (numberOfLevels.get(i).getSelectedItem().equals("3")) {
-						species.get(i).get(3).setText("");
-						species.get(i).get(3).setEnabled(false);
-						species.get(i).get(4).setText("");
-						species.get(i).get(4).setEnabled(false);
-					} else if (numberOfLevels.get(i).getSelectedItem().equals(
-							"4")) {
-						species.get(i).get(3).setEnabled(true);
-						species.get(i).get(4).setText("");
-						species.get(i).get(4).setEnabled(false);
-					} else {
-						species.get(i).get(3).setEnabled(true);
-						species.get(i).get(4).setEnabled(true);
-					}
-				}
-			}
-		}
 		// if the close menu item is selected
-		else if (e.getSource() == close) {
+		if (e.getSource() == close) {
 			frame.dispose();
 		} else if (e.getSource() == user) {
-			for (Component c : speciesPanel.getComponents()) {
-				c.setEnabled(true);
-			}
 			levels();
 		} else if (e.getSource() == auto) {
 			for (Component c : speciesPanel.getComponents()) {
-				c.setEnabled(false);
+				for (Component d : ((JPanel) c).getComponents()) {
+					d.setEnabled(false);
+				}
 			}
 		}
 		// if the browse initial network button is clicked
 		else if (e.getSource() == browseInit) {
-			Buttons.browse(frame, new File(initNetwork.getText().trim()),
-					initNetwork, JFileChooser.FILES_ONLY, "Open");
+			Buttons.browse(frame, new File(initNetwork.getText().trim()), initNetwork,
+					JFileChooser.FILES_ONLY, "Open");
 		}
 		// if the browse experiments directory button is clicked
 		else if (e.getSource() == browseExper) {
@@ -307,64 +291,50 @@ public class Learn implements ActionListener {
 			if (experiments.getText().trim().equals("")) {
 				dir = null;
 			} else {
-				dir = new File(experiments.getText().trim() + File.separator
-						+ ".");
+				dir = new File(experiments.getText().trim() + File.separator + ".");
 			}
-			Buttons.browse(frame, dir, experiments,
-					JFileChooser.DIRECTORIES_ONLY, "Open");
+			Buttons.browse(frame, dir, experiments, JFileChooser.DIRECTORIES_ONLY, "Open");
 			levels();
 		}
 		// if the run button is selected
 		else if (e.getSource() == run) {
 			try {
 				String geneNet = "GeneNet";
-				geneNet += " -d " + debug.getSelectedItem();
+				geneNet += " --debug " + debug.getSelectedItem();
 				try {
-					double activation = Double.parseDouble(this.activation
-							.getText().trim());
+					double activation = Double.parseDouble(this.activation.getText().trim());
 					geneNet += " -a " + activation;
-					double repression = Double.parseDouble(this.repression
-							.getText().trim());
+					double repression = Double.parseDouble(this.repression.getText().trim());
 					geneNet += " -r " + repression;
-					double parent = Double.parseDouble(this.parent.getText()
-							.trim());
+					double parent = Double.parseDouble(this.parent.getText().trim());
 					geneNet += " -v " + parent;
-					int windowRising = Integer.parseInt(this.windowRising
-							.getText().trim());
-					geneNet += " -wr " + windowRising;
-					int windowSize = Integer.parseInt(this.windowSize.getText()
-							.trim());
-					geneNet += " -ws " + windowSize;
-					int numBins = Integer.parseInt(this.numBins.getText()
-							.trim());
-					geneNet += " -nb " + numBins;
+					int windowRising = Integer.parseInt(this.windowRising.getText().trim());
+					geneNet += " --windowRisingAmount " + windowRising;
+					int windowSize = Integer.parseInt(this.windowSize.getText().trim());
+					geneNet += " --windowSize " + windowSize;
+					int numBins = Integer.parseInt(this.numBins.getText().trim());
+					geneNet += " --numBins " + numBins;
 					double influenceLevel = Double
 							.parseDouble(this.influenceLevel.getText().trim());
 					geneNet += " -id " + influenceLevel;
-					double relaxIPDelta = Double.parseDouble(this.relaxIPDelta
-							.getText().trim());
-					geneNet += " -rd " + relaxIPDelta;
-					int letNThrough = Integer.parseInt(this.letNThrough
-							.getText().trim());
+					double relaxIPDelta = Double.parseDouble(this.relaxIPDelta.getText().trim());
+					geneNet += " --relaxIPDelta " + relaxIPDelta;
+					int letNThrough = Integer.parseInt(this.letNThrough.getText().trim());
 					geneNet += " --sip_letNThrough " + letNThrough;
+					double background = Double.parseDouble(this.background.getText().trim());
+					geneNet += " -bkf " + background;
 				} catch (Exception e2) {
-					JOptionPane.showMessageDialog(frame,
-							"Must enter numbers into input fields.", "Error",
-							JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(frame, "Must enter numbers into input fields.",
+							"Error", JOptionPane.ERROR_MESSAGE);
 				}
 				if (user.isSelected()) {
-					FileWriter write = new FileWriter(new File(experiments
-							.getText().trim()
+					FileWriter write = new FileWriter(new File(experiments.getText().trim()
 							+ File.separator + "levels.lvl"));
 					for (int i = 0; i < species.size(); i++) {
-						write.write(species.get(i).get(0).getText() + ", "
-								+ species.get(i).get(1).getText() + ", "
-								+ species.get(i).get(2).getText());
-						if (numberOfLevels.get(i).getSelectedItem().equals("4")) {
-							write.write(", " + species.get(i).get(3).getText());
-						} else if (numberOfLevels.get(i).getSelectedItem()
-								.equals("5")) {
-							write.write(", " + species.get(i).get(4).getText());
+						write.write(((JTextField) species.get(i).get(1)).getText().trim());
+						for (int j = 2; j < species.get(i).size(); j++) {
+							write.write(", "
+									+ ((JTextField) species.get(i).get(j)).getText().trim());
 						}
 						write.write("\n");
 					}
@@ -390,16 +360,32 @@ public class Learn implements ActionListener {
 					geneNet += " --output_donotTossChangedInfluenceSingleParents";
 				}
 				Runtime exec = Runtime.getRuntime();
-				Process learn = exec.exec(geneNet + " "
-						+ experiments.getText().trim() + " > run.log");
+				Process learn = exec.exec(geneNet + " " + experiments.getText().trim());
 				learn.waitFor();
-				exec.exec("dotty "
-						+ new File(experiments.getText().trim()
-								+ File.separator + "method.dot")
-								.getAbsolutePath());
+				String output = "";
+				InputStream reb = learn.getInputStream();
+				int read = reb.read();
+				while (read != -1) {
+					output += (char) read;
+					read = reb.read();
+				}
+				if (!output.equals("")) {
+					FileWriter out = new FileWriter(new File(experiments.getText().trim()
+							+ File.separator + "run.log"));
+					out.write(output);
+					System.out.println("Output:\n" + output + "\n");
+				}
+				if (new File(experiments.getText().trim() + File.separator + "method.dot").exists()) {
+					exec
+							.exec("dotty "
+									+ new File(experiments.getText().trim() + File.separator
+											+ "method.dot").getAbsolutePath());
+				} else {
+					JOptionPane.showMessageDialog(frame, "A dot file was not generated."
+							+ "\nPlease see the run.log file.", "Error", JOptionPane.ERROR_MESSAGE);
+				}
 			} catch (Exception e1) {
-				JOptionPane.showMessageDialog(frame,
-						"Unable to learn from data.", "Error",
+				JOptionPane.showMessageDialog(frame, "Unable to learn from data.", "Error",
 						JOptionPane.ERROR_MESSAGE);
 			}
 		}
@@ -409,11 +395,10 @@ public class Learn implements ActionListener {
 		ArrayList<String> str = null;
 		try {
 			Runtime exec = Runtime.getRuntime();
-			Process learn = exec.exec("GeneNet --lvl "
-					+ experiments.getText().trim());
+			Process learn = exec.exec("GeneNet --lvl " + experiments.getText().trim());
 			learn.waitFor();
-			Scanner f = new Scanner(new File(experiments.getText().trim()
-					+ File.separator + "levels.lvl"));
+			Scanner f = new Scanner(new File(experiments.getText().trim() + File.separator
+					+ "levels.lvl"));
 			str = new ArrayList<String>();
 			while (f.hasNextLine()) {
 				str.add(f.nextLine());
@@ -457,12 +442,11 @@ public class Learn implements ActionListener {
 									word += cha;
 								}
 								input.reset();
-							} else if (cha == ',' || cha == ':' || cha == ';'
-									|| cha == '!' || cha == '?' || cha == '\"'
-									|| cha == '\'' || cha == '(' || cha == ')'
-									|| cha == '{' || cha == '}' || cha == '['
-									|| cha == ']' || cha == '<' || cha == '>'
-									|| cha == '*' || cha == '=' || cha == '#') {
+							} else if (cha == ',' || cha == ':' || cha == ';' || cha == '!'
+									|| cha == '?' || cha == '\"' || cha == '\'' || cha == '('
+									|| cha == ')' || cha == '{' || cha == '}' || cha == '['
+									|| cha == ']' || cha == '<' || cha == '>' || cha == '*'
+									|| cha == '=' || cha == '#') {
 								if (!word.equals("") && !word.equals("time")) {
 									try {
 										Integer.parseInt(word);
@@ -479,55 +463,50 @@ public class Learn implements ActionListener {
 				} catch (Exception e1) {
 				}
 				speciesPanel.removeAll();
-				this.species = new ArrayList<ArrayList<JTextField>>();
-				numberOfLevels = new ArrayList<JComboBox>();
-				speciesPanel.setLayout(new GridLayout(species.size(), 6));
+				this.species = new ArrayList<ArrayList<Component>>();
+				speciesPanel.setLayout(new GridLayout(species.size() + 1, 1));
+				int max = 0;
+				if (str != null) {
+					for (String st : str) {
+						String[] getString = st.split(",");
+						max = Math.max(max, getString.length + 1);
+					}
+				}
+				JPanel label = new JPanel(new GridLayout());
+				label.add(new JLabel("Do Not Use"));
+				label.add(new JLabel("Species"));
+				label.add(new JLabel("Number Of Levels"));
+				for (int i = 0; i < max - 3; i++) {
+					label.add(new JLabel("cutoff " + (i + 1)));
+				}
+				speciesPanel.add(label);
 				for (String s : species) {
-					ArrayList<JTextField> specs = new ArrayList<JTextField>();
+					JPanel sp = new JPanel(new GridLayout());
+					ArrayList<Component> specs = new ArrayList<Component>();
+					specs.add(new JCheckBox());
 					specs.add(new JTextField(s));
 					specs.add(new JTextField());
-					specs.add(new JTextField());
-					specs.add(new JTextField());
-					specs.add(new JTextField());
-					String[] options = new String[3];
-					options[0] = "3";
-					options[1] = "4";
-					options[2] = "5";
-					JComboBox box = new JComboBox(options);
-					box.addActionListener(this);
-					box.setActionCommand(s + "box");
-					specs.get(0).setEditable(false);
-					specs.get(3).setEnabled(false);
-					specs.get(4).setEnabled(false);
-					speciesPanel.add(specs.get(0));
-					speciesPanel.add(box);
-					speciesPanel.add(specs.get(1));
-					speciesPanel.add(specs.get(2));
-					speciesPanel.add(specs.get(3));
-					speciesPanel.add(specs.get(4));
+					((JTextField) specs.get(1)).setEditable(false);
+					sp.add(specs.get(0));
+					sp.add(specs.get(1));
+					sp.add(specs.get(2));
 					this.species.add(specs);
-					numberOfLevels.add(box);
 					if (str != null) {
 						for (String st : str) {
 							String[] getString = st.split(",");
 							if (getString[0].trim().equals(s)) {
-								if (getString.length > 2) {
-									specs.get(1).setText(getString[1].trim());
-									specs.get(2).setText(getString[2].trim());
-								}
-								if (getString.length > 3) {
-									specs.get(3).setText(getString[3].trim());
-									specs.get(3).setEnabled(true);
-									box.setSelectedItem("4");
-								}
-								if (getString.length > 4) {
-									specs.get(4).setText(getString[4].trim());
-									specs.get(4).setEnabled(true);
-									box.setSelectedItem("5");
+								if (getString.length >= 2) {
+									((JTextField) specs.get(2)).setText(getString[1].trim());
+									for (int i = 0; i < Integer
+											.parseInt(((JTextField) specs.get(2)).getText()) - 1; i++) {
+										specs.add(new JTextField(getString[i + 2].trim()));
+										sp.add(specs.get(i + 3));
+									}
 								}
 							}
 						}
 					}
+					speciesPanel.add(sp);
 				}
 				speciesPanel.validate();
 			}
