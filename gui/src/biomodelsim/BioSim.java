9e85bd7fe496dadddd8ea243bf9884c33dc40b40
cjmyers <myers@ece.utah.edu>
2007-09-28 18:48:21 +0000

    Changed version to 0.41.

--- gui/src/biomodelsim/BioSim.java
+++ gui/src/biomodelsim/BioSim.java
@@ -202,7 +202,7 @@ public class BioSim implements MouseListener, ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == about) {
 			final JFrame f = new JFrame("About");
-			JLabel bioSim = new JLabel("BioSim 0.4");
+			JLabel bioSim = new JLabel("BioSim 0.41");
 			Font font = bioSim.getFont();
 			font = font.deriveFont(Font.BOLD, 36.0f);
 			bioSim.setFont(font);
