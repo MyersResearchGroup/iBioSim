7c99d3775b08103c1f85dc55baf1561f5f7f058f
cmadsen <>
2008-05-28 18:40:51 +0000

    Worked on parser

---
+++ gui/src/parser/Parser.java
@@ -0,0 +1,18 @@
+package parser;
+
+import java.util.*;
+
+public class Parser {
+
+	protected ArrayList<String> species;
+
+	protected ArrayList<ArrayList<Double>> data;
+
+	public ArrayList<String> getSpecies() {
+		return species;
+	}
+
+	public ArrayList<ArrayList<Double>> getData() {
+		return data;
+	}
+}
