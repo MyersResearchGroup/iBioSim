7b0912afb063a696d55c594de4caf144778697e3
myers <>
2009-04-05 17:16:43 +0000

    update version

--- gui/src/biomodelsim/BioSim.java
+++ gui/src/biomodelsim/BioSim.java
@@ -1810,7 +1810,7 @@ public class BioSim implements MouseListener, ActionListener, MouseMotionListene
 		}
 		else {
 			name = new JLabel("iBioSim", JLabel.CENTER);
-			version = new JLabel("Version 1.1.1", JLabel.CENTER);
+			version = new JLabel("Version 1.1.2", JLabel.CENTER);
 			developers = "Nathan Barker\nKevin Jones\nHiroyuki Kuwahara\n"
 					+ "Curtis Madsen\nChris Myers\nNam Nguyen";
 		}
