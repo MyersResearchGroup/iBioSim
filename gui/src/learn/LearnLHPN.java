ce6b85fba2f1c957d8cef5f0a62d7b12c1b51d07
kjones <>
2009-06-09 19:08:07 +0000

    Fixed bug that caused the incorrect number of divisions to appear at times in the LHPN Learn view.

--- gui/src/learn/LearnLHPN.java
+++ gui/src/learn/LearnLHPN.java
@@ -762,72 +762,20 @@ public class LearnLHPN extends JPanel implements ActionListener, Runnable {
 		editText(0);
 	}
 
-	/*
-	 * private void levelsBin() { if (!directory.equals("")) { // File n = null; //
-	 * for (File f : new File(directory).listFiles()) { // if
-	 * (f.getAbsolutePath().contains(".tsd")) { // n = f; // } // } if (true) { //
-	 * if (n != null) { // ArrayList<String> species = new ArrayList<String>(); //
-	 * try { // InputStream input = new FileInputStream(n); // boolean reading =
-	 * true; // char cha; // while (reading) { // String word = ""; // boolean
-	 * readWord = true; // while (readWord) { // int read = input.read(); // if
-	 * (read == -1) { // reading = false; // readWord = false; // } // cha =
-	 * (char) read; // if (Character.isWhitespace(cha)) { // word += cha; // } //
-	 * else if (cha == ',' || cha == ':' || cha == ';' || cha == '\"' || cha // ==
-	 * '\'' // || cha == '(' || cha == ')' || cha == '[' || cha == ']') { // if
-	 * (!word.equals("") && !word.equals("time")) { // try { //
-	 * Double.parseDouble(word); // } // catch (Exception e2) { //
-	 * species.add(word); // } // } // word = ""; // } // else if (read != -1) { //
-	 * word += cha; // } // } // } // input.close(); // } // catch (Exception
-	 * e1) { // } speciesPanel.removeAll(); this.species = new ArrayList<ArrayList<Component>>();
-	 * speciesPanel.setLayout(new GridLayout(speciesList.size() + 1, 1)); JPanel
-	 * label = new JPanel(new GridLayout()); // label.add(new JLabel("Use"));
-	 * label.add(new JLabel("Species")); label.add(new JLabel("Number Of
-	 * Bins")); for (int i = 0; i < Integer.parseInt((String)
-	 * numBins.getSelectedItem()) - 1; i++) { label.add(new JLabel("Level " + (i +
-	 * 1))); } speciesPanel.add(label); int j = 0; for (String s : speciesList) {
-	 * j++; JPanel sp = new JPanel(new GridLayout()); ArrayList<Component>
-	 * specs = new ArrayList<Component>(); // JCheckBox check = new
-	 * JCheckBox(); // check.setSelected(true); // specs.add(check);
-	 * specs.add(new JTextField(s)); String[] options = { "0", "1", "2", "3",
-	 * "4", "5", "6", "7", "8", "9" }; JComboBox combo = new JComboBox(options);
-	 * combo.setSelectedItem(numBins.getSelectedItem()); specs.add(combo);
-	 * ((JTextField) specs.get(0)).setEditable(false); // sp.add(specs.get(0)); //
-	 * ((JCheckBox) specs.get(0)).addActionListener(this); // ((JCheckBox)
-	 * specs.get(0)).setActionCommand("box" + j); sp.add(specs.get(0));
-	 * sp.add(specs.get(1)); ((JComboBox) specs.get(1)).addActionListener(this);
-	 * ((JComboBox) specs.get(1)).setActionCommand("text" + j);
-	 * this.species.add(specs); for (int i = 0; i < Integer.parseInt((String)
-	 * ((JComboBox) specs.get(1)) .getSelectedItem()) - 1; i++) { specs.add(new
-	 * JTextField("")); sp.add(specs.get(i + 2)); } speciesPanel.add(sp); } } } }
-	 */
-
 	private void editText(int num) {
 		try {
 			ArrayList<Component> specs = variables.get(num);
 			Component[] panels = variablesPanel.getComponents();
 			int boxes = Integer.parseInt((String) ((JComboBox) specs.get(1)).getSelectedItem());
-			//boolean selected = ((JCheckBox) specs.get(1)).isSelected();
 			if ((specs.size() - 2) < boxes) {
-				for (int i = 0; i < boxes - 2; i++) {
+				for (int i = 0; i < boxes - 1; i++) {
 					try {
-						JTextField text = (JTextField) specs.get(i + 2);
-						//if (selected) {
-						//	text.setEditable(false);
-						//}
-						//else {
-							text.setEditable(true);
-						//}
+						specs.get(i + 2);
 					}
 					catch (Exception e1) {
 						JTextField temp = new JTextField("");
 						((JPanel) panels[num + 1]).add(temp);
 						specs.add(temp);
-						//if (selected) {
-						//	temp.setEditable(false);
-						//}
-						//else {
-							temp.setEditable(true);
-						//}
 					}
 				}
 			}
@@ -841,8 +789,8 @@ public class LearnLHPN extends JPanel implements ActionListener, Runnable {
 					}
 					else if (boxes == 0) {
 						while (true) {
-							specs.remove(1);
-							((JPanel) panels[num + 1]).remove(1);
+							specs.remove(2);
+							((JPanel) panels[num + 1]).remove(2);
 						}
 					}
 				}
@@ -854,12 +802,12 @@ public class LearnLHPN extends JPanel implements ActionListener, Runnable {
 				max = Math.max(max, variables.get(i).size());
 			}
 			if (((JPanel) panels[0]).getComponentCount() < max) {
-				for (int i = 0; i < max - 1; i++) {
+				for (int i = 0; i < max - 2; i++) {
 					try {
-						((JPanel) panels[0]).getComponent(i + 1);
+						((JPanel) panels[0]).getComponent(i + 2);
 					}
 					catch (Exception e) {
-						((JPanel) panels[0]).add(new JLabel("Level " + (i)));
+						((JPanel) panels[0]).add(new JLabel("Level " + (i + 1)));
 					}
 				}
 			}
@@ -885,7 +833,7 @@ public class LearnLHPN extends JPanel implements ActionListener, Runnable {
 					}
 				}
 				else {
-					for (int j = sp.getComponentCount() - 1; j >= max; j--) {
+					for (int j = sp.getComponentCount() - 2; j >= max; j--) {
 						sp.remove(j);
 					}
 				}
