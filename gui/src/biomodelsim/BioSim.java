e7bc1767be9c61ee3a9f694ce2828adb69bb5da2
kjones <>
2009-10-05 22:39:48 +0000

    Fixed bug viewing log of verification and synthesis views.

--- gui/src/biomodelsim/BioSim.java
+++ gui/src/biomodelsim/BioSim.java
@@ -2019,14 +2019,11 @@ public class BioSim implements MouseListener, ActionListener, MouseMotionListene
 							JOptionPane.ERROR_MESSAGE);
 				}
 			}
-			else if (comp instanceof JPanel) {
-				Component[] array = ((JPanel) comp).getComponents();
-				if (array[0] instanceof Verification) {
-					((Verification) array[0]).viewLog();
-				}
-				else if (array[0] instanceof Synthesis) {
-					((Synthesis) array[0]).viewLog();
-				}
+			else if (comp instanceof Verification) {
+				((Verification) comp).viewLog();
+			}
+			else if (comp instanceof Synthesis) {
+				((Synthesis) comp).viewLog();
 			}
 			else if (comp instanceof JTabbedPane) {
 				Component component = ((JTabbedPane) comp).getSelectedComponent();
