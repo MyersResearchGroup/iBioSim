be4b842c752c12a1bed671108885ed3800696b3f
myers <>
2009-10-27 15:30:15 +0000

    Removed override line.

--- gui/src/learn/Variable.java
+++ gui/src/learn/Variable.java
@@ -120,7 +120,7 @@ public class Variable implements Comparable<Variable>{
 		initValue_vMin *=  vScaleFactor;
 	}
 	
-	@Override
+	//@Override
 	public int compareTo(Variable o) {
 		return (this.getName().compareToIgnoreCase(o.getName()));
 	}
