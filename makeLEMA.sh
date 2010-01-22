95aebe712eeeb20498501363537bc626091c363e
cjmyers <myers@ece.utah.edu>
2010-01-22 19:59:45 +0000

    Added mac make build scripts

---
+++ makeLEMA.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cp bin/*.py /Applications/LEMA.app/Contents/Resources/bin/.
+cp bin/*.pyc /Applications/LEMA.app/Contents/Resources/bin/.
+cp bin/atacs /Applications/LEMA.app/Contents/Resources/bin/.
+cp lib/*.dylib /Applications/LEMA.app/Contents/Resources/lib/.
+cp lib/*.jnilib /Applications/LEMA.app/Contents/Resources/lib/.
+cp docs/*.html /Applications/LEMA.app/Contents/Resources/docs/.
+cp docs/*.pdf /Applications/LEMA.app/Contents/Resources/docs/.
+cp docs/screenshots /Applications/LEMA.app/Contents/Resources/docs/screenshots/.
+cp gui/lib/* /Applications/LEMA.app/Contents/Resources/gui/lib/.
+cp gui/icons/* /Applications/LEMA.app/Contents/Resources/gui/icons/.
+cp gui/bin/* /Applications/LEMA.app/Contents/Resources/gui/bin/.
+cp gui/dist/lib/* /Applications/LEMA.app/Contents/Resources/gui/dist/lib/.
+cp -r gui/dist/classes/* /Applications/LEMA.app/Contents/Resources/gui/dist/classes/.
+
+
---
+++ makedmg.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cp bin/*.pl /Applications/iBioSim.app/Contents/Resources/bin/.
+cp bin/reb2sac /Applications/iBioSim.app/Contents/Resources/bin/.
+cp bin/GeneNet /Applications/iBioSim.app/Contents/Resources/bin/.
+cp lib/*.dylib /Applications/iBioSim.app/Contents/Resources/lib/.
+cp lib/*.jnilib /Applications/iBioSim.app/Contents/Resources/lib/.
+cp docs/*.html /Applications/iBioSim.app/Contents/Resources/docs/.
+cp docs/*.pdf /Applications/iBioSim.app/Contents/Resources/docs/.
+cp docs/screenshots /Applications/iBioSim.app/Contents/Resources/docs/screenshots/.
+cp gui/lib/* /Applications/iBioSim.app/Contents/Resources/gui/lib/.
+cp gui/icons/* /Applications/iBioSim.app/Contents/Resources/gui/icons/.
+cp gui/bin/* /Applications/iBioSim.app/Contents/Resources/gui/bin/.
+cp gui/dist/lib/* /Applications/iBioSim.app/Contents/Resources/gui/dist/lib/.
+cp -r gui/dist/classes/* /Applications/iBioSim.app/Contents/Resources/gui/dist/classes/.
+
+
