ee9cb3579e3746000db77fdce862862a95938d38
kjones <>
2010-04-06 18:12:10 +0000

    Re-write of LHPN data structure done.  Now based on separate classes for places and transitions.

---
+++ gui/src/lhpn2sbml/parser/Variable.java
@@ -0,0 +1,81 @@
+package lhpn2sbml.parser;
+
+import java.util.Properties;
+
+public class Variable {
+	
+	private String name;
+	
+	private String type;
+	
+	private String initValue;
+	
+	private String initRate;
+	
+	private boolean output = true;
+	
+	public Variable(String name, String type) {
+		this.name = name;
+		this.type = type;
+	}
+	
+	public Variable(String name, String type, String initValue) {
+		this.name = name;
+		this.type = type;
+		this.initValue = initValue;
+	}
+	
+	public Variable(String name, String type, Properties initCond) {
+		if (type.equals(CONTINUOUS)) {
+			this.name = name;
+			this.type = type;
+			this.initValue = initCond.getProperty("value");
+			this.initRate = initCond.getProperty("rate");
+		}
+	}
+	
+	public Variable(String name, String type, String initValue, boolean output) {
+		this.name = name;
+		this.type = type;
+		this.initValue = initValue;
+		this.output = output;
+	}
+	
+	public void addInitValue(String initValue) {
+		this.initValue = initValue;
+	}
+	
+	public void addInitRate(String initRate) {
+		this.initRate = initRate;
+	}
+	
+	public void addInitCond(Properties initCond) {
+		if (type.equals(CONTINUOUS)) {
+			this.initValue = initCond.getProperty("value");
+			this.initRate = initCond.getProperty("rate");
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getInitValue() {
+		return initValue;
+	}
+	
+	public String getInitRate() {
+		return initRate;
+	}
+	
+	public boolean isOutput() {
+		return output;
+	}
+	
+	public static final String BOOLEAN = "boolean";
+	
+	public static final String INTEGER = "integer";
+	
+	public static final String CONTINUOUS = "continuous";
+	
+}
