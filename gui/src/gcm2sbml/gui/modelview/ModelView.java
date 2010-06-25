09e57d149f3d37d185e28b9b3c8656fbc7ae994e
tyler <>
2010-06-25 22:04:00 +0000

    adding influence adds it to existing influences panel

--- gui/src/gcm2sbml/gui/modelview/ModelView.java
+++ gui/src/gcm2sbml/gui/modelview/ModelView.java
@@ -2,6 +2,7 @@ package gcm2sbml.gui.modelview;
 
 import gcm2sbml.gui.InfluencePanel;
 import gcm2sbml.gui.PropertiesLauncher;
+import gcm2sbml.util.GlobalConstants;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
@@ -13,6 +14,9 @@ import java.util.HashMap;
 import java.util.Properties;
 
 
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
 import javax.swing.JComponent;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
@@ -68,26 +72,48 @@ public class ModelView extends JPanel implements ActionListener {
 			});
 
 //			// listener for added verticies
-//			graph.addListener(mxEvent.CELLS_ADDED, new mxEventSource.mxIEventListener() {
-//				@Override
-//				public void invoke(Object arg0, mxEventObject event) {
-//					Object cells[] = (Object [])event.getProperties().get("cells");
-//					
-//					System.out.print(event.getName()+"\n");
-//					
-//					if(cells.length == 1 && ((mxCell)(cells[0])).isEdge()){
-//						String isBio = "no";
-//						String type = "activation";
-//						String promoter = "default";
-//						String parentId = "";
-//						String childId = "";
-//						String name = InfluencePanel.buildName(parentId, childId, type, isBio, promoter);
-//						
-//						String a = "ASDF";
-//					}
-//
-//				}
-//			});
+			graph.addListener(mxEvent.CELLS_ADDED, new mxEventSource.mxIEventListener() {
+				@Override
+				public void invoke(Object arg0, mxEventObject event) {
+					
+					// if the graph is building, ignore the creation of edges.
+					if(graph.is_building == false){
+					
+						Object cells[] = (Object [])event.getProperties().get("cells");
+						
+						if(cells.length == 1 && ((mxCell)(cells[0])).isEdge()){
+		
+							mxCell edge = (mxCell)(cells[0]);
+							
+							String isBio;
+							String type;
+							String constType;
+							if(activation_button.isSelected()){
+								isBio = "no"; type = "activation"; constType = GlobalConstants.ACTIVATION;
+							}else if(inhibition_button.isSelected()){
+								isBio = "no"; type = "inhibition"; constType = GlobalConstants.REPRESSION;
+							}else if(bio_activation_button.isSelected()){
+								isBio = "yes"; type = "activation"; constType = GlobalConstants.ACTIVATION;
+							}else if(bio_inhibition_button.isSelected()){
+								isBio = "yes"; type = "inhibition"; constType = GlobalConstants.REPRESSION;
+							}else if(no_influence_button.isSelected()){
+								isBio = ""; type = "no influence"; constType = GlobalConstants.NOINFLUENCE;
+							}else{
+								throw(new Error("No influence button was pressed!"));
+							}
+							
+							String promoter = "default";
+							String parentId = edge.getSource().getId();
+							String childId = edge.getTarget().getId();
+							String name = InfluencePanel.buildName(parentId, childId, type, isBio, promoter);
+							
+							graph.addInfluence(edge, name, constType);
+							PropertiesLauncher.getInstance().addInfluenceToList(name);
+						}
+					}
+
+				}
+			});
 			
 			
 			
@@ -142,6 +168,11 @@ public class ModelView extends JPanel implements ActionListener {
 	 * create the toolbar.
 	 * @return
 	 */
+	AbstractButton activation_button;
+	AbstractButton inhibition_button;
+	AbstractButton bio_activation_button;
+	AbstractButton bio_inhibition_button;
+	AbstractButton no_influence_button;
 	private JToolBar buildToolBar(){
 
 		JToolBar toolBar = new JToolBar();
@@ -150,6 +181,20 @@ public class ModelView extends JPanel implements ActionListener {
 		toolBar.add(Utils.makeToolButton("add_species.png", "addSpecies", "Add Species", this));
 		toolBar.add(Utils.makeToolButton("", "addInfluence", "Add Influence", this));
 
+		ButtonGroup influenceButtonGroup = new ButtonGroup();
+		
+		activation_button = Utils.makeRadioToolButton("activation.png", "", "Create Activation Influences", this, influenceButtonGroup);
+		activation_button.setSelected(true);
+		toolBar.add(activation_button);
+		inhibition_button = Utils.makeRadioToolButton("inhibition.png", "", "Create Repression Influences", this, influenceButtonGroup);
+		toolBar.add(inhibition_button);
+		bio_activation_button = Utils.makeRadioToolButton("bio_activation.png", "", "Create Biological Activation Influences", this, influenceButtonGroup);
+		toolBar.add(bio_activation_button);
+		bio_inhibition_button = Utils.makeRadioToolButton("bio_inhibition.png", "", "Create Biological Repression Influences", this, influenceButtonGroup);
+		toolBar.add(bio_inhibition_button);
+		no_influence_button = Utils.makeRadioToolButton("no_influence.png", "", "Explicitly Set No Influences", this, influenceButtonGroup);
+		toolBar.add(no_influence_button);
+		
 		return toolBar;
 	}
 
@@ -183,6 +228,8 @@ public class ModelView extends JPanel implements ActionListener {
 			PropertiesLauncher.getInstance().launchInfluencePanel(null);
 			
 			refreshGraph();
+		}else if(command == ""){
+			// radio buttons don't have to do anything and have an action command of "".
 		}else{
 			throw(new Error("Invalid actionCommand: " + command));
 		}
