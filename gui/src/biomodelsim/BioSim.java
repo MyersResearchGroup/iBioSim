cdc7917865388a9e846e15d521536e62547c3353
cjmyers <myers@ece.utah.edu>
2010-11-08 16:23:51 +0000

    Changed version to 1.5.1.

--- gui/src/biomodelsim/BioSim.java
+++ gui/src/biomodelsim/BioSim.java
@@ -2061,20 +2061,20 @@ public class BioSim implements MouseListener, ActionListener, MouseMotionListene
 		final String developers;
 		if (lema) {
 			name = new JLabel("LEMA", JLabel.CENTER);
-			version = new JLabel("Version 1.5", JLabel.CENTER);
+			version = new JLabel("Version 1.5.1", JLabel.CENTER);
 			developers = "Satish Batchu\nKevin Jones\nScott Little\nCurtis Madsen\nChris Myers\nNicholas Seegmiller\n"
 					+ "Robert Thacker\nDavid Walter";
 		}
 		else if (atacs) {
 			name = new JLabel("ATACS", JLabel.CENTER);
-			version = new JLabel("Version 6.5", JLabel.CENTER);
+			version = new JLabel("Version 6.5.1", JLabel.CENTER);
 			developers = "Wendy Belluomini\nJeff Cuthbert\nHans Jacobson\nKevin Jones\nSung-Tae Jung\n"
 					+ "Christopher Krieger\nScott Little\nCurtis Madsen\nEric Mercer\nChris Myers\n"
 					+ "Curt Nelson\nEric Peskin\nNicholas Seegmiller\nDavid Walter\nHao Zheng";
 		}
 		else {
 			name = new JLabel("iBioSim", JLabel.CENTER);
-			version = new JLabel("Version 1.5", JLabel.CENTER);
+			version = new JLabel("Version 1.5.1", JLabel.CENTER);
 			developers = "Nathan Barker\nKevin Jones\nHiroyuki Kuwahara\n"
 					+ "Curtis Madsen\nChris Myers\nNam Nguyen\nNicolas Roehner\nTyler Patterson";
 		}
