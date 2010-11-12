3ccf54a24bc3a676c278cf264081499fc6903592
kjones <>
2010-11-12 05:10:31 +0000

    Fixed more bugs saving and loading abstraction ordering.

--- gui/src/verification/AbstPane.java
+++ gui/src/verification/AbstPane.java
@@ -77,7 +77,7 @@ public class AbstPane extends JPanel implements ActionListener, Runnable {
 			xform29 = "Remove Uninteresting Variables - simplification";
 
 	public String[] transforms = { xform0, xform1, xform3, xform4, xform5, xform6, xform7, xform8,
-			xform25, xform9, xform24, xform10, xform12, xform14, xform16, xform11,
+			xform25, xform9, xform24, xform10, xform12, xform13, xform14, xform16, xform11,
 			xform15, xform17, xform18, xform19, xform20, xform21, xform22, xform23, xform26, xform27, xform28, xform29 };
 
 	public JTextField factorField, iterField;
