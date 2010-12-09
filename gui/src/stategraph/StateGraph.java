d306c43744122312667bf16b88a1ae9b261f5606
cmadsen <>
2010-12-09 23:16:12 +0000

    Fixed the way that conditions are displayed in the histogram when performing transient Markov chain analysis.

--- gui/src/stategraph/StateGraph.java
+++ gui/src/stategraph/StateGraph.java
@@ -293,19 +293,15 @@ public class StateGraph implements Runnable {
 			double prob = 0;
 			for (String state : stateGraph.keySet()) {
 				for (State m : stateGraph.get(state)) {
-					ExprTree expr1 = new ExprTree(lhpn);
-					ExprTree expr2 = new ExprTree(lhpn);
-					expr1.token = expr1.intexpr_gettok("~(" + condition[0] + ")");
-					expr2.token = expr2.intexpr_gettok("~(" + condition[1] + ")");
-					expr1.intexpr_L("~(" + condition[0] + ")");
-					expr2.intexpr_L("~(" + condition[1] + ")");
-					if (expr1.evaluateExpr(m.getVariables()) == 1.0
-							&& expr2.evaluateExpr(m.getVariables()) == 1.0) {
+					expr = new ExprTree(lhpn);
+					expr.token = expr.intexpr_gettok("~(" + condition[0] + ")");
+					expr.intexpr_L("~(" + condition[0] + ")&~(" + condition[1] + ")");
+					if (expr.evaluateExpr(m.getVariables()) == 1.0) {
 						prob += m.getCurrentProb();
 					}
 				}
 			}
-			output.put("~(" + condition[0].trim() + ")&&~(" + condition[1].trim() + ")", prob);
+			output.put("~(" + condition[0].trim() + ")&~(" + condition[1].trim() + ")", prob);
 			totalProb += prob;
 			prob = 0;
 			for (String state : stateGraph.keySet()) {
