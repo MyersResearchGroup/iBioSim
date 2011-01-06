bd1a1c54edf90bd736219488bb2ea9bf627ccfe4
cmadsen <>
2011-01-06 10:05:55 +0000

    Added more level selections to the interesting species panel.

--- gui/src/reb2sac/Reb2Sac.java
+++ gui/src/reb2sac/Reb2Sac.java
@@ -4772,7 +4772,7 @@ public class Reb2Sac extends JPanel implements ActionListener, Runnable, MouseLi
 			check.setSelected(false);
 			specs.add(check);
 			specs.add(new JTextField((String) s));
-			String[] options = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+			String[] options = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
 			JComboBox combo = new JComboBox(options);
 			combo.setEnabled(false);
 			specs.add(combo);
