3fe2f8d649db7f9d8e6b26f635a9e96a67a9af58
cmadsen <>
2011-02-12 01:49:22 +0000

    Updated color button ui.

--- gui/src/graph/Graph.java
+++ gui/src/graph/Graph.java
@@ -107,7 +107,6 @@ import com.lowagie.text.pdf.DefaultFontMapper;
 import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfTemplate;
 import com.lowagie.text.pdf.PdfWriter;
-import com.sun.java.swing.plaf.windows.WindowsBorders;
 import com.sun.java.swing.plaf.windows.WindowsButtonUI;
 
 /**
@@ -4395,7 +4394,6 @@ public class Graph extends JPanel implements ActionListener, MouseListener, Char
 			colorsCombo.add(colBox);
 			JButton colorButton = new JButton();
 			colorButton.setUI(new WindowsButtonUI());
-			colorButton.setBorder(WindowsBorders.getToolBarBorder());
 			colorButton.setBackground((Color) colory.get("Black"));
 			colorButton.setForeground((Color) colory.get("Black"));
 			colorButton.setActionCommand("" + i);
@@ -7773,7 +7771,6 @@ public class Graph extends JPanel implements ActionListener, MouseListener, Char
 			colorsCombo.add(colBox);
 			JButton colorButton = new JButton();
 			colorButton.setUI(new WindowsButtonUI());
-			colorButton.setBorder(WindowsBorders.getToolBarBorder());
 			colorButton.setBackground((Color) colory.get("Black"));
 			colorButton.setForeground((Color) colory.get("Black"));
 			colorButton.setActionCommand("" + i);
