afeeadbe5eccb3ed362f30eb6dedf044fc6ca80d
myers <>
2011-03-04 15:33:55 +0000

    Removed unused import.

--- gui/src/gcm/gui/modelview/movie/visualizations/gradient/GradientDisplayer.java
+++ gui/src/gcm/gui/modelview/movie/visualizations/gradient/GradientDisplayer.java
@@ -9,7 +9,6 @@ import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.GroupLayout.Alignment;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
