55f7876ee847bbe6b89ffc8fc23ade50929dca17
cmadsen <>
2011-04-14 13:49:54 +0000

    Added the none option to analysis views when using properties.

--- gui/src/lpn/parser/LhpnFile.java
+++ gui/src/lpn/parser/LhpnFile.java
@@ -272,8 +272,8 @@ public class LhpnFile {
 				}
 			}
 			if (properties != null && !properties.isEmpty()) {
-				if (!properties.contains("none"))
-					properties.add(0, "none");
+//				if (!properties.contains("none"))
+//					properties.add(0, "none");
 				for (String property : properties) {
 					buffer.append("#@.property ");
 					buffer.append(property + "\n");
