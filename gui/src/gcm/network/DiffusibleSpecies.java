c2dd92dbdbb9fa55c336a4f1977da49d0ff3e7e4
jstev <>
2011-07-09 18:31:52 +0000

    removed membrane diffusion rate accessor because it's now in AbstractSpecies

--- gui/src/gcm/network/DiffusibleSpecies.java
+++ gui/src/gcm/network/DiffusibleSpecies.java
@@ -5,11 +5,8 @@ import gcm.visitor.SpeciesVisitor;
 
 public class DiffusibleSpecies extends AbstractSpecies{
 	
-	public double getKmdiff() {
-		return Double.parseDouble(getProperty(GlobalConstants.MEMDIFF_STRING));
-	}
-	
 	public void accept(SpeciesVisitor visitor) {
 		visitor.visitDiffusibleSpecies(this);
+		
 	}
 }
