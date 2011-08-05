d56382b1b7044049688c6461b33872cb083cb40e
jstev <>
2011-08-05 18:32:11 +0000

    rubberband bugfix.  i also added a key that you can hold down to zoom/pan (ie, alt).

--- gui/src/gcm/gui/Grid.java
+++ gui/src/gcm/gui/Grid.java
@@ -996,18 +996,21 @@ public class Grid {
 	 */
 	private void selectGridLocationsWithRubberband(Graphics g) {
 		
+		rubberbandBounds.y += verticalOffset;
+		
 		//loop through all of the grid locations
 		//if its rectangle is contained within the rubberband, select it
 		for (int row = 0; row < numRows; ++row) {
 			for (int col = 0; col < numCols; ++col) {
 				
-				if (rubberbandBounds.contains(grid.get(row).get(col).getZoomedSnapRectangle()))
+				if (rubberbandBounds.contains(grid.get(row).get(col).getZoomedRectangle()))
 					grid.get(row).get(col).setSelected(true);
 			}
 		}
 		
 		mouseReleased = false;
 		rubberbandBounds = null;
+		mouseClicked = false;
 	}	
 	
 	
