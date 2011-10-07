e32d91e37346ee8cfb9d50ea5e29445f0f4be3df
myers <>
2011-10-07 15:52:43 +0000

    Use serial garbage collector.

--- bin/iBioSim.bat
+++ bin/iBioSim.bat
@@ -1,5 +1,5 @@
 setlocal ENABLEDELAYEDEXPANSION
 set CLASSPATH="%BIOSIM%\gui\dist\classes"
 for /f %%a IN ('dir /b "%BIOSIM%\gui\lib\*.jar"') do call set CLASSPATH=!CLASSPATH!;"%BIOSIM%\gui\lib\%%a"
-java -Xmx512M -classpath %CLASSPATH% main.Gui
+java -Xmx2048M -Xms2048M -XX:+UseSerialGC -classpath %CLASSPATH% main.Gui
 
