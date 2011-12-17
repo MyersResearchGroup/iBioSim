37fe19b96f4d9860d8e9750ab753660c2c9d912e
andrewf <>
2011-12-17 06:17:28 +0000

    Andrew N. Fisher ECE 6750 project.

---
+++ gui/src/verification/timed_state_exploration/IDBM.java
@@ -0,0 +1,11 @@
+package verification.timed_state_exploration;
+
+/**
+ * This interface gives the method that are needed for the StateExploration pertaining
+ * to the difference bound matrices.
+ * @author Andrew N. Fisher
+ *
+ */
+public interface IDBM {
+
+}
