732c09147a1be496b2f9a87d6ca21551119d5fb7
andrewf <>
2012-02-02 14:34:54 +0000

    Minor changes.

--- gui/src/verification/timed_state_exploration/zone/TimedState.java
+++ gui/src/verification/timed_state_exploration/zone/TimedState.java
@@ -3,10 +3,18 @@ package verification.timed_state_exploration.zone;
 import lpn.parser.LhpnFile;
 import verification.platu.stategraph.State;
 
+/**
+ * Adds timing to a State.
+ * 
+ * @author Andrew N. Fisher
+ *
+ */
 public class TimedState extends State{
 	
+	/* A Zone for keeping track timing information. */
 	Zone _zone;
 
+	
 	public TimedState(LhpnFile lpn, int[] new_marking, int[] new_vector,
 			boolean[] new_isTranEnabled) {
 		super(lpn, new_marking, new_vector, new_isTranEnabled);
