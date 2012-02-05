9d947558cefea7101caafdb76ceef310c2d86549
andrewf <>
2012-02-05 05:17:59 +0000

    Some minor implementations of Zone methods and adding fire and enable method stubs for the StateGraph_timed.

--- gui/src/verification/timed_state_exploration/zone/StateGraph_timed.java
+++ gui/src/verification/timed_state_exploration/zone/StateGraph_timed.java
@@ -1,6 +1,9 @@
 package verification.timed_state_exploration.zone;
 
 import lpn.parser.LhpnFile;
+import lpn.parser.Transition;
+import verification.platu.lpn.LpnTranList;
+import verification.platu.stategraph.State;
 import verification.platu.stategraph.StateGraph;
 
 public class StateGraph_timed extends StateGraph{
@@ -9,5 +12,21 @@ public class StateGraph_timed extends StateGraph{
 		super(lpn);
 		// TODO Auto-generated constructor stub
 	}
+	
+	
+	
+	public LpnTranList getEnabled(State curState)
+	{
+		// TODO: Overrides the base state and must add checks for with zones.
+		// Note : Can also just make a new method signature with the TimedState
+		return null;
+	}
+	
+	 public State[] fire(final StateGraph[] curSgArray, final State[] curStateArray,
+			 Transition firedTran)
+	 {
+		 // TODO: Overrides the base and must add the zone factor.
+		 return null;
+	 }
 
 }
