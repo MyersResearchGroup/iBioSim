87ec010dc492824e1f185e8e03c6d54882fd7bdd
zhangz <>
2012-02-15 04:02:52 +0000

    Major changes: 1. Added a new constructor Component(). 2. Added several getters. 3. Added buildLPN method that takes a component and convert it into an LPN.

--- gui/src/lpn/parser/Component.java
+++ gui/src/lpn/parser/Component.java
@@ -3,25 +3,125 @@ package lpn.parser;
 import java.util.ArrayList;
 
 public class Component extends LhpnFile{
-	private ArrayList<LpnProcess> processes = new ArrayList<LpnProcess>();
-	private ArrayList<Transition> compTrans = new ArrayList<Transition>();
-	private ArrayList<Place> compPlaces = new ArrayList<Place>();
-	private ArrayList<Variable> compInput = new ArrayList<Variable>();
-	private ArrayList<Variable> compOutput = new ArrayList<Variable>();
-	private ArrayList<Variable> compInternal = new ArrayList<Variable>();
-	private int id;
+	private ArrayList<Integer> processIDList;
+	private ArrayList<Transition> compTrans;
+	private ArrayList<Place> compPlaces; 
+	private ArrayList<Variable> compInputs;
+	private ArrayList<Variable> compOutputs;
+	private ArrayList<Variable> compInternals;
+	private int compid;
 	
 	public Component(LpnProcess process) {
-		processes.add(process);
+		compTrans = new ArrayList<Transition>();
+		compPlaces = new ArrayList<Place>();
+		processIDList = new ArrayList<Integer>();
+		compInputs = new ArrayList<Variable>();
+		compOutputs = new ArrayList<Variable>();
+		compInternals = new ArrayList<Variable>();
+		
+		processIDList.add(process.getProcessId());
 		compTrans.addAll(process.getProcessTransitions());
 		compPlaces.addAll(process.getProcessPlaces());
-		compInput.addAll(process.getProcessInput());
-		compOutput.addAll(process.getProcessOutput());
-		compInternal.addAll(process.getProcessInternal());	
+		compInputs.addAll(process.getProcessInput());
+		compOutputs.addAll(process.getProcessOutput());
+		compInternals.addAll(process.getProcessInternal());	
+		compid = process.getProcessId();
+	}
+
+	public Component() {
+		//compTrans = new ArrayList<Transition>();
+		//compPlaces = new ArrayList<Place>();
+		processIDList = new ArrayList<Integer>();
+		compInputs = new ArrayList<Variable>();
+		compOutputs = new ArrayList<Variable>();
+		compInternals = new ArrayList<Variable>();
 	}
 	
 	public Component getComponent() {
 		return this;
 	}
+
+	public Integer getComponentId() {
+		return compid;
+	}
+	
+	public void setComponentId(Integer id) {
+		this.compid = id;
+	}
+
+	public ArrayList<Integer> getProcessIDList() {
+		return processIDList;
+	}
+	
+	public void setProcessIDList(ArrayList processIDList) {
+		this.processIDList = processIDList;
+	}
+
+	public ArrayList<Variable> getInternals() {
+		return compInternals;
+	}
+
+	public ArrayList<Variable> getOutputs() {
+		return compOutputs;
+	}
+
+	public ArrayList<Variable> getInputs() {
+		return compInputs;
+	}
+
+	public LhpnFile buildLPN(LhpnFile lpnComp) {
+		// Places
+		for (int i=0; i< this.getComponentPlaces().size(); i++) {
+			Place p = this.getComponentPlaces().get(i);
+			lpnComp.addPlace(p.getName(), p.isMarked());
+		}
+		// Transitions
+		for (int i=0; i< this.getCompTransitions().size(); i++) {
+			Transition t = this.getCompTransitions().get(i);
+			lpnComp.addTransition(t);
+		}
+		// Inputs
+		for (int i=0; i< this.getCompInput().size(); i++) {
+			Variable var = this.getCompInput().get(i);
+			lpnComp.addInput(var.getName(), var.getType(), var.getInitValue());
+		}
+		// Outputs
+		for (int i=0; i< this.getCompOutput().size(); i++) {
+			Variable var = this.getCompOutput().get(i);
+			lpnComp.addOutput(var.getName(), var.getType(), var.getInitValue());
+		}
+		// Internal
+		for (int i=0; i< this.getCompInternal().size(); i++) {
+			Variable var = this.getCompInternal().get(i);
+			lpnComp.addInternal(var.getName(), var.getType(), var.getInitValue());
+		}
+		return lpnComp;		
+	}
+
+	private ArrayList<Variable> getCompInternal() {
+		return compInternals;
+	}
+
+	private ArrayList<Variable> getCompOutput() {
+		return compOutputs;
+	}
+
+	private ArrayList<Variable> getCompInput() {
+		return compInputs;
+	}
+
+	private ArrayList<Transition> getCompTransitions() {
+		return compTrans;
+	}
+
+	private ArrayList<Place> getComponentPlaces() {
+		return compPlaces;
+	}
+	
+	public int getNumVars() {
+		// return the number of variables in this component
+		return compInputs.size() + compInternals.size() + compOutputs.size();
+	}
+	
 
 }
