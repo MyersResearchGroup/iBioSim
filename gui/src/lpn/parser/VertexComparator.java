f31fcf4da89f6a4f9a37f67da0c40587facfcd9b
zgzn <moschino21@gmail.com>
2012-02-15 04:16:41 +0000

    modified the compare method.

--- gui/src/lpn/parser/VertexComparator.java
+++ gui/src/lpn/parser/VertexComparator.java
@@ -3,9 +3,18 @@ package lpn.parser;
 import java.util.Comparator;
 
 public class VertexComparator implements Comparator<Vertex>{
-	public int compare(Vertex v1, Vertex v2) {
-		return 0;
-		
+	private int maxNumVarsInOneComp;
+	
+	public VertexComparator(Integer maxNumVarsInOneComp) {
+		this.maxNumVarsInOneComp = maxNumVarsInOneComp;
 	}
 
+	public int compare(Vertex v1, Vertex v2) {
+		if (v1.calculateBestNetGain(maxNumVarsInOneComp) >= v2.calculateBestNetGain(maxNumVarsInOneComp)) 
+			return -1;
+		else //if (v1.calculateBestNetGain(maxNumVarsInOneComp) > v2.calculateBestNetGain(maxNumVarsInOneComp)) 
+			return 1;
+//		else
+//			return 0;
+	}
 }
