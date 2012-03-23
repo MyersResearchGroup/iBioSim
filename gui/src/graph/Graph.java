e7051a5de809ffc5db9e2b989773dd8d94fe8933
cmadsen <>
2012-03-23 00:00:42 +0000

    Working on bifurcation code.

--- gui/src/graph/Graph.java
+++ gui/src/graph/Graph.java
@@ -4041,7 +4041,8 @@ public class Graph extends JPanel implements ActionListener, MouseListener, Char
 	private JPanel fixGraphChoices(final String directory) {
 		if (directory.equals("")) {
 			if (selected.equals("Average") || selected.equals("Variance") || selected.equals("Standard Deviation")
-					|| selected.equals("Termination Time") || selected.equals("Percent Termination") || selected.equals("Constraint Termination")) {
+					|| selected.equals("Termination Time") || selected.equals("Percent Termination") || selected.equals("Constraint Termination")
+					|| selected.equals("Bifurcation Statistics")) {
 				if (selected.equals("Average")
 						&& new File(outDir + separator + "mean" + "." + printer_id.substring(0, printer_id.length() - 8)).exists()) {
 					readGraphSpecies(outDir + separator + "mean" + "." + printer_id.substring(0, printer_id.length() - 8));
