542ac6517ccc3af687217e6dc80058b5dcb9d2bd
cmadsen <>
2012-06-04 23:43:19 +0000

    Added a Median Path method.

--- gui/src/main/Gui.java
+++ gui/src/main/Gui.java
@@ -1419,7 +1419,7 @@ public class Gui implements MouseListener, ActionListener, MouseMotionListener, 
 				choices = new String[] { "euler", "gear1", "gear2", "rk4imp", "rk8pd", "rkf45" };
 			}
 			else if (type.getSelectedItem().equals("Monte Carlo")) {
-				choices = new String[] { "gillespie", "gillespieJava", "Gillespie SSA-CR (Java)", "Gillespie SSA-Direct (Java)", "mpde", "mp", "mp-bifurcation", "mp-adaptive", "mp-adaptive-bifurcation", "mp-event", "mp-event-bifurcation", "emc-sim", "bunker", "nmc"};
+				choices = new String[] { "gillespie", "gillespieJava", "Gillespie SSA-CR (Java)", "Gillespie SSA-Direct (Java)", "mpde", "mp", "mp-median", "mp-bifurcation", "mp-adaptive", "mp-adaptive-median", "mp-adaptive-bifurcation", "mp-event", "mp-event-median", "mp-event-bifurcation", "emc-sim", "bunker", "nmc"};
 			}
 			else if (type.getSelectedItem().equals("Markov")) {
 				choices = new String[] { "steady-state-markov-chain-analysis", "transient-markov-chain-analysis", "reachability-analysis", "atacs",
@@ -1492,10 +1492,13 @@ public class Gui implements MouseListener, ActionListener, MouseMotionListener, 
 						sim.addItem("Gillespie SSA-Direct (Java)");
 						sim.addItem("mpde");
 						sim.addItem("mp");
+						sim.addItem("mp-median");
 						sim.addItem("mp-bifurcation");
 						sim.addItem("mp-adaptive");
+						sim.addItem("mp-adaptive-median");
 						sim.addItem("mp-adaptive-bifurcation");
 						sim.addItem("mp-event");
+						sim.addItem("mp-event-median");
 						sim.addItem("mp-event-bifurcation");
 						sim.addItem("emc-sim");
 						sim.addItem("bunker");
