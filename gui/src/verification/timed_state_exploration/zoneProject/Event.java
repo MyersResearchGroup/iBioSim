098e07c8430221d8ecca36c8756bb4da5bf99e74
andrewf <>
2012-09-28 17:32:57 +0000

    Complete implementing method stubs except things with rates.

--- gui/src/verification/timed_state_exploration/zoneProject/Event.java
+++ gui/src/verification/timed_state_exploration/zoneProject/Event.java
@@ -2,17 +2,43 @@ package verification.timed_state_exploration.zoneProject;
 
 import lpn.parser.Transition;
 
+/**
+ * An Event is an action that is pending. This can be a transition to fire or an inequality.
+ * @author Andrew N. Fisher
+ *
+ */
 public class Event {
 
+	/*
+	 * Abstraction Function : An event simply represents a transition or an Inequality. Whichever the 
+	 * event is representing is stored in the corresponding member variables _transition or _inequality.
+	 */
 	
+	/*
+	 * Representation Invariant : Exactly one of _transition or _inequality should be non-null at a given
+	 * time.
+	 */
 	
+	Transition _transition;
+	
+	InequalityVariable _inequality;
+	
+	/**
+	 * Initializes the Event as a transition event.
+	 * @param t
+	 * 		The transition the event represents.
+	 */
 	public Event(Transition t){
-		
+		_transition = t;
 	}
 	
-	
+	/**
+	 * Initializes the Event as an inequality event.
+	 * @param v
+	 * 		The inequality this event represents.
+	 */
 	public Event(InequalityVariable v){
-		
+		_inequality = v;
 	}
 	
 	/**
@@ -22,30 +48,44 @@ public class Event {
 	 */
 	public boolean isTransition() {
 		
-		return false;
+		return _transition != null;
 	}
 
 	
 	/**
-	 * Determines whether the EventSet represents a rate event.
+	 * Determines whether this Event represents a rate event.
 	 * @return
-	 * 		True if this EventSet represents a rate event; flase otherwise.
+	 * 		True if this EventSet represents a rate event; false otherwise.
 	 */
 	public boolean isRate() {
 		
 		return false;
 	}
 	
+	/**
+	 * Determines whether this Event represents an inequality.
+	 * @return
+	 * 		True if this Event is an inequality, false otherwise.	
+	 */
 	public boolean isInequality(){
-		return false;
+		return _inequality != null;
 	}
 	
-	
+	/**
+	 * Gets the inequality variable that this Event represents.
+	 * @return
+	 * 		The inequality variable if this Event represents an inequality, null otherwise.
+	 */
 	public InequalityVariable getInequalityVariable(){
-		return null;
+		return _inequality;
 	}
 	
+	/**
+	 * Gets the transition that this Event represents.
+	 * @return
+	 * 		The transition if this Event represents a transition, null otherwise.
+	 */
 	public Transition getTransition(){
-		return null;
+		return _transition;
 	}
 }
