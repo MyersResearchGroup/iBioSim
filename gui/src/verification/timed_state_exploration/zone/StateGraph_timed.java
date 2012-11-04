b87e6a0537db0c9ceedb2d3545011f2f52ee1f69
andrewf <>
2012-11-04 15:12:40 +0000

    Removed null parameters from being passed to the fire methods.

--- gui/src/verification/timed_state_exploration/zone/StateGraph_timed.java
+++ gui/src/verification/timed_state_exploration/zone/StateGraph_timed.java
@@ -48,7 +48,7 @@ public class StateGraph_timed extends StateGraph{
 	 {
 		 // TODO: Overrides the base and must add the zone factor.
 		 
-		 State[] newState = super.fire(curSgArray, curStateArray, firedTran, null, null);
+		 State[] newState = super.fire(curSgArray, curStateArray, firedTran);
 		 //TimedState[] newTimedStates = new TimedState[newState.length];
 		 State[] newTimedStates = new State[newState.length];
 		 
