3620881a1db2cfe452a7c18ce53dbb17c4ca4cfa
zhangz <>
2013-05-21 00:05:13 +0000

    Commented out field variable str2ObjTbl and methods that use it.

--- gui/src/verification/platu/common/IndexObjMap.java
+++ gui/src/verification/platu/common/IndexObjMap.java
@@ -7,12 +7,13 @@ import verification.platu.common.PlatuObj;
 public class IndexObjMap<T extends PlatuObj> {
 	
 	protected HashMap<T, T> uniqueObjTbl;
-	protected HashMap<String, T> str2ObjTbl;
+	// TODO: (temp) str2ObjTbl is not used. 
+//	protected HashMap<String, T> str2ObjTbl;
 	protected HashMap<Integer, T> idx2ObjTbl;
 	
 	public IndexObjMap() {
 		uniqueObjTbl = new HashMap<T, T>();
-		str2ObjTbl = new HashMap<String, T>();
+//		str2ObjTbl = new HashMap<String, T>();
 		idx2ObjTbl = new HashMap<Integer, T>();
 	}
 
@@ -23,13 +24,13 @@ public class IndexObjMap<T extends PlatuObj> {
 		
 		this.uniqueObjTbl.put(obj, obj);
 		
-		String objLabel = obj.getLabel();
-		if(objLabel != null){
-			if(this.str2ObjTbl.containsKey(objLabel) == false)
-				this.str2ObjTbl.put(obj.getLabel(), obj);
-			else
-				;// Thrown an exception
-		}
+//		String objLabel = obj.getLabel();
+//		if(objLabel != null){
+//			if(this.str2ObjTbl.containsKey(objLabel) == false)
+//				this.str2ObjTbl.put(obj.getLabel(), obj);
+//			else
+//				;// Throw an exception
+//		}
 		
 		int idx = this.idx2ObjTbl.size();
 		obj.setIndex(idx);
@@ -37,9 +38,9 @@ public class IndexObjMap<T extends PlatuObj> {
 		return obj;
 	}
 	
-	public T get(String label) {
-		return this.str2ObjTbl.get(label);
-	}
+//	public T get(String label) {
+//		return this.str2ObjTbl.get(label);
+//	}
 	
 	public T get(Integer index) {
 		return this.idx2ObjTbl.get(index);
