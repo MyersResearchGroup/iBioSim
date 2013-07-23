4aae269180d122385c20d3a9fb8d7ea9b3c7e28d
glass <>
2013-07-23 22:11:36 +0000

    Added JOptimizer Dependencies jars in gui>lib

--- gui/src/analysis/FluxBalanceAnalysis.java
+++ gui/src/analysis/FluxBalanceAnalysis.java
@@ -73,37 +73,37 @@ public class FluxBalanceAnalysis {
 		
 		
 		
-//		// Objective function (plane)
-//		LinearMultivariateRealFunction objectiveFunction = new LinearMultivariateRealFunction(new double[] { -1., -1. }, 4);
-//
-//		//inequalities (polyhedral feasible set G.X<H )
-//		ConvexMultivariateRealFunction[] inequalities = new ConvexMultivariateRealFunction[4];
-//		double[][] G = new double[][] {{4./3., -1}, {-1./2., 1.}, {-2., -1.}, {1./3., 1.}};
-//		double[] H = new double[] {2., 1./2., 2., 1./2.};
-//		inequalities[0] = new LinearMultivariateRealFunction(G[0], -H[0]);
-//		inequalities[1] = new LinearMultivariateRealFunction(G[1], -H[1]);
-//		inequalities[2] = new LinearMultivariateRealFunction(G[2], -H[2]);
-//		inequalities[3] = new LinearMultivariateRealFunction(G[3], -H[3]);
-//		
-//		//optimization problem
-//		OptimizationRequest or = new OptimizationRequest();
-//		or.setF0(objectiveFunction);
-//		or.setFi(inequalities);
-//		//or.setInitialPoint(new double[] {0.0, 0.0});//initial feasible point, not mandatory
-//		or.setToleranceFeas(1.E-9);
-//		or.setTolerance(1.E-9);
-//		
-//		//optimization
-//		jop = new JOptimizer();
-//		jop.setOptimizationRequest(or);
-//		int returnCode = 0;
-//		try {
-//			returnCode = jop.optimize();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		double [] sol = jop.getOptimizationResponse().getSolution();
-//		return (int) sol[0];
-//	}
-	
+		// Objective function (plane)
+		LinearMultivariateRealFunction objectiveFunction = new LinearMultivariateRealFunction(new double[] { -1., -1. }, 4);
+
+		//inequalities (polyhedral feasible set G.X<H )
+		ConvexMultivariateRealFunction[] inequalities = new ConvexMultivariateRealFunction[4];
+		double[][] G = new double[][] {{4./3., -1}, {-1./2., 1.}, {-2., -1.}, {1./3., 1.}};
+		double[] H = new double[] {2., 1./2., 2., 1./2.};
+		inequalities[0] = new LinearMultivariateRealFunction(G[0], -H[0]);
+		inequalities[1] = new LinearMultivariateRealFunction(G[1], -H[1]);
+		inequalities[2] = new LinearMultivariateRealFunction(G[2], -H[2]);
+		inequalities[3] = new LinearMultivariateRealFunction(G[3], -H[3]);
+		
+		//optimization problem
+		OptimizationRequest or = new OptimizationRequest();
+		or.setF0(objectiveFunction);
+		or.setFi(inequalities);
+		//or.setInitialPoint(new double[] {0.0, 0.0});//initial feasible point, not mandatory
+		or.setToleranceFeas(1.E-9);
+		or.setTolerance(1.E-9);
+		
+		//optimization
+		jop = new JOptimizer();
+		jop.setOptimizationRequest(or);
+		int returnCode = 0;
+		try {
+			returnCode = jop.optimize();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		double [] sol = jop.getOptimizationResponse().getSolution();
+		return (int) sol[0];
+	}
+
 }
