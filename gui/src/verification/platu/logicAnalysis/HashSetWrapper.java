8b1ec5b34ba5382205080fe9e3c268ddb1a065a2
zgzn <moschino21@gmail.com>
2013-07-31 22:50:21 +0000

    Added a get method.

--- gui/src/verification/platu/logicAnalysis/HashSetWrapper.java
+++ gui/src/verification/platu/logicAnalysis/HashSetWrapper.java
@@ -17,4 +17,18 @@ public class HashSetWrapper extends HashSet<PrjState> implements StateSetInterfa
 	public boolean add(PrjState state) {
 		return super.add(state);
 	}
+	
+	/**
+	 * This method takes a PrjState instance otherPrjState, iterates through this hash set of PrjState instances, 
+	 * and grabs the one from this set that "equals" to otherPrjState. If no match is found, this method returns null.
+	 * @param otherPrjState
+	 * @return
+	 */
+	public PrjState get(PrjState otherPrjState) {		
+		for (PrjState prjSt : this) {
+			if (this.contains(otherPrjState))
+				return prjSt;
+		}
+		return null;
+	}
 }
