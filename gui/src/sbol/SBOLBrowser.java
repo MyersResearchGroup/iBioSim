24d820c94d2077e8101f7687a3777d3f05984b16
myers <>
2013-10-15 21:17:28 +0000

    Added checks that types are present befroe going through them.

--- gui/src/sbol/SBOLBrowser.java
+++ gui/src/sbol/SBOLBrowser.java
@@ -167,7 +167,11 @@ public class SBOLBrowser extends JPanel implements ActionListener {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			dnacTypes.add(SBOLUtility.convertURIToSOType(localComp.getTypes().iterator().next()));
+			if (localComp.getTypes().size()>0) {
+				if (localComp.getTypes().size()>0) {	
+					dnacTypes.add(SBOLUtility.convertURIToSOType(localComp.getTypes().iterator().next()));
+				}
+			}
 		}
 		filterBox = new JComboBox(dnacTypes.toArray(new String[dnacTypes.size()]));
 		
