a9cc578871e794fd2215e834591b38469f6b6e0f
cjmyers <myers@ece.utah.edu>
2013-11-19 19:49:10 +0000

    Script to make atacs for mac

---
+++ makeATACS64.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+cp bin/*.py /Applications/ATACS.app/Contents/Resources/bin/.
+cp bin/*.pyc /Applications/ATACS.app/Contents/Resources/bin/.
+cp bin/atacs /Applications/ATACS.app/Contents/Resources/bin/.
+cp bin/parg /Applications/ATACS.app/Contents/Resources/bin/.
+cp bin/reb2sac.mac64 /Applications/iBioSim.app/Contents/Resources/bin/reb2sac
+cp lib64/*.dylib /Applications/ATACS.app/Contents/Resources/lib/.
+cp lib64/*.jnilib /Applications/ATACS.app/Contents/Resources/lib/.
+cp docs/*.html /Applications/ATACS.app/Contents/Resources/docs/.
+cp docs/*.pdf /Applications/ATACS.app/Contents/Resources/docs/.
+cp docs/screenshots /Applications/ATACS.app/Contents/Resources/docs/screenshots/.
+cp gui/lib/* /Applications/ATACS.app/Contents/Resources/gui/lib/.
+cp gui/icons/* /Applications/ATACS.app/Contents/Resources/gui/icons/.
+cp gui/icons/modelview/* /Applications/ATACS.app/Contents/Resources/gui/icons/modelview/.
+cp gui/icons/modelview/movie/* /Applications/ATACS.app/Contents/Resources/gui/icons/modelview/movie/.
+cp gui/bin/* /Applications/ATACS.app/Contents/Resources/gui/bin/.
+cp -r gui/dist/classes/* /Applications/ATACS.app/Contents/Resources/gui/dist/classes/.
+
+
