fab021e3dba2b721451a37a316f3871fff60ade2
cjmyers <myers@ece.utah.edu>
2014-05-02 19:25:46 +0000

    Changed to SBase fo action listener on index boxes.

--- gui/src/biomodel/gui/sbmlcore/Rules.java
+++ gui/src/biomodel/gui/sbmlcore/Rules.java
@@ -1172,7 +1172,7 @@ public class Rules extends JPanel implements ActionListener, MouseListener {
 		// if the variable is changed
 		else if (e.getSource() == ruleVar) {
 			//TODO: Check the variable and enable/disable indices accordingly.
-			Parameter variable = (Parameter) SBMLutilities.getElementBySId(bioModel.getSBMLDocument(), (String)ruleVar.getSelectedItem());
+			SBase variable = (SBase) SBMLutilities.getElementBySId(bioModel.getSBMLDocument(), (String)ruleVar.getSelectedItem());
 			String[] sizes = new String[2];
 			sizes[0] = AnnotationUtility.parseVectorSizeAnnotation(variable);
 			if(sizes[0]==null){
