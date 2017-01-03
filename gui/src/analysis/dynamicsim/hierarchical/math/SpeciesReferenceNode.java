69674347372e2d69597bfa45c6c836c112e37e71
leandrohw <leandrohw@gmail.com>
2017-01-03 06:45:59 +0000

    Rearranged code to for more efficient hierarchical simulation

---
+++ gui/src/analysis/dynamicsim/hierarchical/math/SpeciesReferenceNode.java
@@ -0,0 +1,56 @@
+package analysis.dynamicsim.hierarchical.math;
+
+public class SpeciesReferenceNode extends VariableNode
+{
+	private SpeciesNode	species;
+	private String		speciesRefId;
+
+	public SpeciesReferenceNode()
+	{
+		super("none");
+	}
+
+	public SpeciesReferenceNode(String id, double value)
+	{
+		super(id);
+	}
+
+	public SpeciesReferenceNode(SpeciesReferenceNode copy)
+	{
+		super(copy);
+		this.species = copy.species.clone();
+		this.speciesRefId = copy.speciesRefId;
+	}
+
+	public void setSpecies(SpeciesNode species)
+	{
+		this.species = species;
+	}
+
+	public void setSpeciesRefId(String species)
+	{
+		this.speciesRefId = species;
+	}
+
+	public String getSpeciesRefId()
+	{
+		return speciesRefId;
+	}
+
+	public double getStoichiometry(int index)
+	{
+		return getValue(index);
+	}
+
+	public SpeciesNode getSpecies()
+	{
+		return species;
+	}
+
+	@Override
+	public SpeciesReferenceNode clone()
+	{
+		return new SpeciesReferenceNode(this);
+	}
+
+}
--- gui/src/analysis/dynamicsim/hierarchical/states/ArrayValue.java
+++
@@ -1,6 +0,0 @@
-package analysis.dynamicsim.hierarchical.states;
-
-public class ArrayValue
-{
-
-}
