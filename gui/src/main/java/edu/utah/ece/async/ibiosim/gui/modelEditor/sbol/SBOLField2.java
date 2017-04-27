b90941731cc15fb28f01a3f994558ed65af645bd
Michael Zhang <michael13162@gmail.com>
2017-04-27 13:35:41 -0600

    Incorporated PartEditDialog to Associate SBOL, buggy

--- gui/src/main/java/edu/utah/ece/async/ibiosim/gui/modelEditor/sbol/SBOLField2.java
+++ gui/src/main/java/edu/utah/ece/async/ibiosim/gui/modelEditor/sbol/SBOLField2.java
@@ -19,6 +19,7 @@ import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
 //import java.net.URISyntaxException;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -30,18 +31,22 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.sbolstandard.core2.ComponentDefinition;
 import org.sbolstandard.core2.SBOLConversionException;
 import org.sbolstandard.core2.SBOLDocument;
+import org.sbolstandard.core2.SBOLReader;
 import org.sbolstandard.core2.SBOLValidationException;
+import org.sbolstandard.core2.SBOLWriter;
 
 import edu.utah.ece.async.ibiosim.dataModels.sbol.SBOLUtility;
 import edu.utah.ece.async.ibiosim.dataModels.util.GlobalConstants;
 import edu.utah.ece.async.ibiosim.gui.Gui;
 import edu.utah.ece.async.ibiosim.gui.modelEditor.schematic.ModelEditor;
 import edu.utah.ece.async.ibiosim.gui.util.EditPreferences;
+import edu.utah.ece.async.sboldesigner.sbol.editor.dialog.PartEditDialog;
 
 public class SBOLField2 extends JPanel implements ActionListener {
-	
+
 	/**
 	 * 
 	 */
@@ -56,25 +61,25 @@ public class SBOLField2 extends JPanel implements ActionListener {
 	private ModelEditor modelEditor;
 	private boolean isModelPanelField;
 	private URI removedBioSimURI;
-	
-	public SBOLField2(List<URI> sbolURIs, String sbolStrand, String sbolType, ModelEditor modelEditor, 
-			int styleOption, boolean isModelPanelField) {
+
+	public SBOLField2(List<URI> sbolURIs, String sbolStrand, String sbolType, ModelEditor modelEditor, int styleOption,
+			boolean isModelPanelField) {
 		super(new GridLayout(1, styleOption));
 		this.sbolURIs.addAll(sbolURIs);
 		this.sbolStrand = sbolStrand;
 		constructField(sbolType, modelEditor, styleOption, isModelPanelField);
 	}
-	
+
 	public SBOLField2(String sbolType, ModelEditor modelEditor, int styleOption, boolean isModelPanelField) {
 		super(new GridLayout(1, styleOption));
 		sbolStrand = GlobalConstants.SBOL_ASSEMBLY_PLUS_STRAND;
 		constructField(sbolType, modelEditor, styleOption, isModelPanelField);
 	}
-	
+
 	public void constructField(String sbolType, ModelEditor modelEditor, int styleOption, boolean isModelPanelField) {
 		this.sbolType = sbolType;
 		this.styleOption = styleOption;
-		if (styleOption == 2 || styleOption  == 3) {
+		if (styleOption == 2 || styleOption == 3) {
 			setLabel(sbolType);
 			this.add(sbolLabel);
 		}
@@ -84,46 +89,71 @@ public class SBOLField2 extends JPanel implements ActionListener {
 		if (styleOption == 3)
 			this.add(sbolText);
 		sbolText.setVisible(false);
-		
+
 		this.modelEditor = modelEditor;
 		this.isModelPanelField = isModelPanelField;
 	}
-	
+
 	public List<URI> getSBOLURIs() {
 		return sbolURIs;
 	}
-	
+
 	public void setSBOLURIs(List<URI> sbolURIs) {
 		this.sbolURIs = sbolURIs;
 	}
-	
-	
+
 	public String getSBOLStrand() {
 		return sbolStrand;
 	}
-	
+
 	public void setSBOLStrand(String sbolStrand) {
 		this.sbolStrand = sbolStrand;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("associateSBOL")) {
 			HashSet<String> sbolFilePaths = modelEditor.getGui().getFilePaths(GlobalConstants.SBOL_FILE_EXTENSION);
-			SBOLAssociationPanel2  associationPanel;
-			if (isModelPanelField) 
-			{
-				associationPanel = new SBOLAssociationPanel2(sbolFilePaths, sbolURIs, sbolStrand, 
-						SBOLUtility.soSynonyms(sbolType), modelEditor);
-				removedBioSimURI = associationPanel.getRemovedBioSimURI();
-			} else
-				associationPanel = new SBOLAssociationPanel2(sbolFilePaths, sbolURIs, sbolStrand,
-						SBOLUtility.soSynonyms(sbolType));
-			sbolURIs = associationPanel.getComponentURIs();
-			sbolStrand = associationPanel.getComponentStrand();
-		} 
+
+			if (sbolURIs.size() > 0) {
+				// use PartEditDialog to edit part
+				try {
+					String filePath = sbolFilePaths.iterator().next();
+					SBOLDocument workingDoc = SBOLReader.read(filePath);
+					ComponentDefinition cd = workingDoc.getComponentDefinition(sbolURIs.get(0));
+					if (cd == null) {
+						JOptionPane.showMessageDialog(getParent(), filePath + " cannot be found in this project.");
+						return;
+					}
+
+					// TODO save is kind of buggy, especially with renaming
+					ComponentDefinition editedCD = PartEditDialog.editPart(getParent(), cd, true, true, workingDoc);
+
+					sbolURIs = Arrays.asList(editedCD.getIdentity());
+					SBOLWriter.write(workingDoc, filePath);
+
+				} catch (SBOLValidationException | IOException | SBOLConversionException e1) {
+					e1.printStackTrace();
+				}
+			} else {
+				// use SBOLAssociationPanel2 to add part
+				SBOLAssociationPanel2 associationPanel;
+
+				if (isModelPanelField) {
+					associationPanel = new SBOLAssociationPanel2(sbolFilePaths, sbolURIs, sbolStrand,
+							SBOLUtility.soSynonyms(sbolType), modelEditor);
+					removedBioSimURI = associationPanel.getRemovedBioSimURI();
+				} else {
+					associationPanel = new SBOLAssociationPanel2(sbolFilePaths, sbolURIs, sbolStrand,
+							SBOLUtility.soSynonyms(sbolType));
+				}
+
+				sbolURIs = associationPanel.getComponentURIs();
+				sbolStrand = associationPanel.getComponentStrand();
+			}
+		}
 	}
-	
+
 	private void setLabel(String sbolType) {
 		if (sbolType.equals(GlobalConstants.SBOL_COMPONENTDEFINITION)) {
 			if (styleOption == 3)
@@ -139,8 +169,9 @@ public class SBOLField2 extends JPanel implements ActionListener {
 		else if (sbolType.equals(GlobalConstants.SBOL_TERMINATOR))
 			sbolLabel = new JLabel("SBOL Terminator");
 	}
-	
-	// Deletes from local SBOL files any iBioSim composite component that had its URI removed from the SBOLAssociationPanel
+
+	// Deletes from local SBOL files any iBioSim composite component that had
+	// its URI removed from the SBOLAssociationPanel
 	public void deleteRemovedBioSimComponent() throws SBOLValidationException {
 		if (removedBioSimURI != null) {
 			for (String filePath : modelEditor.getGui().getFilePaths(GlobalConstants.SBOL_FILE_EXTENSION)) {
@@ -159,13 +190,13 @@ public class SBOLField2 extends JPanel implements ActionListener {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
-			
+
 			removedBioSimURI = null;
 		}
 	}
-	
+
 	public void resetRemovedBioSimURI() {
 		removedBioSimURI = null;
 	}
