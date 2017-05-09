47e7a6445df34bd0d69b4161a526e0bcca090510
Leandro <leandrohw@gmail.com>
2017-05-09 15:59:57 -0600

    Definition of some constants used in many places in analysis.

---
+++ analysis/src/main/java/edu/utah/ece/async/ibiosim/analysis/properties/PropertiesConstants.java
@@ -0,0 +1,56 @@
+package edu.utah.ece.async.ibiosim.analysis.properties;
+
+public class PropertiesConstants {
+
+	public static final String complex_formation_abstraction = "complex-formation-and-sequestering-abstraction";
+	public static final String operator_site_abstraction = "operator-site-reduction-abstraction";
+	public static final String gcm_Abstraction = "gcm.abstraction.method.";
+	
+
+	public static final String reb2sac_abstraction = "reb2sac.abstraction.method";
+	public static final String reb2sac_abstraction_1 = "reb2sac.abstraction.method.1.";
+	public static final String reb2sac_abstraction_2 = "reb2sac.abstraction.method.2.";
+	public static final String reb2sac_abstraction_3 = "reb2sac.abstraction.method.3.";
+	public static final String reb2sac_interesting_species = "reb2sac.interesting.species.";
+	public static final String reb2sac_concentration_level = "reb2sac.concentration.level.";
+	public static final String reb2sac_rapid_equil_1 = "reb2sac.rapid.equilibrium.condition.1";
+	public static final String reb2sac_rapid_equil_2 = "reb2sac.rapid.equilibrium.condition.2";
+	public static final String reb2sac_qssa_1 = "reb2sac.qssa.condition.1";
+	public static final String reb2sac_max_operator_thresghld = "reb2sac.operator.max.concentration.threshold";
+	public static final String reb2sac_diff_stoich_ampl = "reb2sac.diffusion.stoichiometry.amplification.value";
+	public static final String reb2sac_stats = "reb2sac.generate.statistics";
+	public static final String reb2sac_transform = "abstraction.transform.";
+	public static final String reb2sac_simulation = "reb2sac.simulation.method";
+	public static final String reb2sac_issa_paths = "reb2sac.iSSA.number.paths";
+	public static final String reb2sac_issa_type = "reb2sac.iSSA.type";
+	public static final String reb2sac_issa_adaptive = "reb2sac.iSSA.adaptive";
+	
+
+	public static final String sim_printer = "simulation.printer";
+	public static final String sim_tracjing_quantity = "simulation.printer.tracking.quantity";
+	public static final String simulation_init_time = "simulation.initial.time";
+	public static final String simulation_out_time = "simulation.output.start.time";
+	public static final String simulation_run_term = "simulation.run.termination.decider";
+	
+	public static final String ode_time_limit = "ode.simulation.time.limit";
+	public static final String ode_print_interval = "ode.simulation.print.interval";
+	public static final String ode_min_print_interval = "ode.simulation.minimum.print.interval";
+	public static final String ode_number_steps = "ode.simulation.number.steps";
+	public static final String ode_time_step = "ode.simulation.time.step";
+	public static final String ode_min_time_step = "ode.simulation.min.time.step";
+	public static final String ode_abs_error = "ode.simulation.absolute.error";
+	public static final String ode_rel_error = "ode.simulation.relative.error";
+	public static final String ode_out_dir = "ode.simulation.out.dir";
+	
+	public static final String mc_seed = "monte.carlo.simulation.random.seed";
+	public static final String mc_runs = "monte.carlo.simulation.runs";
+	public static final String mc_time_limit = "monte.carlo.simulation.time.limit";
+	public static final String mc_print_interval = "monte.carlo.simulation.print.interval";
+	public static final String mc_min_print = "monte.carlo.simulation.minimum.print.interval";
+	public static final String mc_number_steps = "monte.carlo.simulation.number.steps";
+	public static final String mc_time_step = "monte.carlo.simulation.time.step";
+	public static final String mc_min_time_step = "monte.carlo.simulation.min.time.step";
+	public static final String mc_random_seed = "monte.carlo.simulation.random.seed";
+	public static final String mc_simulation_runs = "monte.carlo.simulation.runs";
+	public static final String mc_out_dir = "monte.carlo.simulation.out.dir";
+}
