9562b07aa6dd4ba695ccf52794087f482c827bec
Tramy Nguyen <tramy.nguy@gmail.com>
2017-07-23 22:52:13 -0600

    created test case and command line access for VPR model generation

--- conversion/src/test/java/edu/utah/ece/async/ibiosim/conversion/ModelGeneratorTests.java
+++ conversion/src/test/java/edu/utah/ece/async/ibiosim/conversion/ModelGeneratorTests.java
@@ -4,9 +4,17 @@ import java.io.File;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.sbolstandard.core2.DirectionType;
+import org.sbolstandard.core2.FunctionalComponent;
+import org.sbolstandard.core2.ModuleDefinition;
 import org.sbolstandard.core2.SBOLConversionException;
 import org.sbolstandard.core2.SBOLDocument;
+import org.sbolstandard.core2.SBOLReader;
 import org.sbolstandard.core2.SBOLValidationException;
+
+import junit.framework.Assert;
+import uk.ac.ncl.ico2s.VPRException;
+import uk.ac.ncl.ico2s.VPRTripleStoreException;
 
 // TODO: temporarily removed until VPR library sorted
 //import uk.ac.ncl.ico2s.VPRException;
@@ -20,39 +28,94 @@ public class ModelGeneratorTests extends ConversionAbstractTests{
 	 * r3.xml - module_BO_11205_activates_BO_27654; module_BO_28532_encodes_BO_26966
 	 * r4.xml - Pro: BO_2685 RBS: BO_27789 CDS: BO_28531 T: BO_4261
 	 */
-	// TODO: temporarily removed until VPR library sorted
-/*
-	@Test
-	public void test_repressions() throws VPRException{
-		File inputFolder = new File(repressionDir);
-		File[] listOfFiles = inputFolder.listFiles();
-		int index = 1;
-		for (int i = 0; i < listOfFiles.length; i++) {
-		      if (listOfFiles[i].isFile()) {
-		        System.out.println("File " + listOfFiles[i].getAbsolutePath());
-		        SBOLDocument outDoc = null;
-				try {
-					outDoc = ModelGenerator.generateModel(listOfFiles[i]);
-				} catch (SBOLValidationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (SBOLConversionException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (VPRException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (VPRTripleStoreException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		        
-		        ModelGenerator.exportModel("/Users/tramyn/Desktop/outputModels/" + "r"+ index++ + "_out.xml", outDoc);
-		      } 
-		    }
+	
+	private SBOLDocument readSBOLFile(String fullInputFileName)
+	{
+		File file = new File(fullInputFileName);
+		SBOLDocument inSBOLDoc = null;
+		try {
+			inSBOLDoc = SBOLReader.read(file);
+		} catch (SBOLValidationException e) {
+			System.err.println("ERROR: Invalid SBOL file.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("ERROR: Unable to read file.");
+			e.printStackTrace();
+		} catch (SBOLConversionException e) {
+			System.err.println("ERROR: Unable to perform internal SBOL conversion from libSBOLj library.");
+			e.printStackTrace();
+		}
+		
+		return inSBOLDoc;
 	}
-	*/
+	
+	@Test
+	public void test_file()
+	{
+		String fileName = "inverterExample"; 
+		String inputfile = sbolDir + fileName + ".xml";
+		String selectedRepo = "https://synbiohub.org";
+		String uriPrefix = "http://www.async.ece.utah.edu/";
+		
+		String[] cmdArgs = {"-u", uriPrefix, "-r", selectedRepo, inputfile};
+		edu.utah.ece.async.ibiosim.conversion.VPRModelGenerator.main(cmdArgs);
+	}
+	
+	@Test
+	public void test_port()
+	{
+		String fileName = "inverterExample"; 
+		String inputfile = sbolDir + fileName + ".xml";
+		String uriPrefix = "http://www.async.ece.utah.edu/";
+		String selectedRepo = "https://synbiohub.org";
+		
+		SBOLDocument inSBOL = readSBOLFile(inputfile);
+		if(inSBOL == null)
+		{
+			System.err.println("ERROR: Invalid SBOL file.");
+			return;
+		}
+		try 
+		{
+			SBOLDocument generatedSBOL = edu.utah.ece.async.ibiosim.conversion.VPRModelGenerator.generateModel(selectedRepo, inSBOL);
+			for(ModuleDefinition modDef : generatedSBOL.getModuleDefinitions())
+			{
+				for(FunctionalComponent funcComp : modDef.getFunctionalComponents())
+				{
+					if(funcComp.getDisplayId().equals("BO_11410") || funcComp.getDisplayId().equals("BO_10845"))
+					{
+						Assert.assertTrue(funcComp.getDirection().equals(DirectionType.IN) 
+								|| funcComp.getDirection().equals(DirectionType.OUT)
+								|| funcComp.getDirection().equals(DirectionType.NONE));
+					}
+				}
+			}
+		} 
+		catch (SBOLValidationException e) 
+		{
+			System.err.println("ERROR: Invalid SBOL file.");
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			System.err.println("ERROR: Unable to read file.");
+			e.printStackTrace();
+		} 
+		catch (SBOLConversionException e) 
+		{
+			System.err.println("ERROR: Unable to perform internal SBOL conversion from libSBOLj library.");
+			e.printStackTrace();
+		} 
+		catch (VPRException e) 
+		{
+			System.err.println("ERROR: Unable to Perform VPR Model Generation");
+			e.printStackTrace();
+		} 
+		catch (VPRTripleStoreException e) 
+		{
+			System.err.println("ERROR: This SBOL file has contents that can't perform VPR model generation.");
+			e.printStackTrace();
+		}
+	}
+	
 }
