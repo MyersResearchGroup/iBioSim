5e716008328a6adc8a0e5997fe33ade59cad5399
leandrohw <leandrohw@gmail.com>
2018-06-27 13:15:48 -0600

    Fixed issue with printing time

--- analysis/src/main/java/edu/utah/ece/async/ibiosim/analysis/simulation/hierarchical/io/HierarchicalTSDWriter.java
+++ analysis/src/main/java/edu/utah/ece/async/ibiosim/analysis/simulation/hierarchical/io/HierarchicalTSDWriter.java
@@ -28,58 +28,51 @@ import edu.utah.ece.async.ibiosim.analysis.simulation.hierarchical.math.Hierarch
  */
 public class HierarchicalTSDWriter extends HierarchicalWriter {
 
-	private final StringBuilder header;
+  private final StringBuilder header;
 
-	public HierarchicalTSDWriter () {
-		super();
-		header = new StringBuilder();
-	}
+  public HierarchicalTSDWriter() {
+    super();
+    header = new StringBuilder();
+    header.append("(\"time\"");
+  }
 
-	@Override
-	public void print () throws IOException {
-		bufferedWriter.write(",\n(");
-		if (listOfStates.size() > 0) {
-			bufferedWriter.write(listOfStates.get(0).toString());
+  @Override
+  public void print(double currentTime) throws IOException {
+    bufferedWriter.write(",\n(");
+    bufferedWriter.write(String.valueOf(currentTime));
+    for (int i = 0; i < this.listOfStates.size(); ++i) {
+      bufferedWriter.write("," + listOfStates.get(i).toString());
+    }
 
-			for (int i = 1; i < this.listOfStates.size(); ++i) {
-				bufferedWriter.write("," + listOfStates.get(i).toString());
-			}
-		}
+    bufferedWriter.write(")");
+    bufferedWriter.flush();
+  }
 
-		bufferedWriter.write(")");
-		bufferedWriter.flush();
-	}
+  @Override
+  public void addVariable(String id, HierarchicalNode node, int index, boolean isConcentration) {
+    header.append(",\"" + id + "\"");
+    addNode(node, index, isConcentration);
+  }
 
-	@Override
-	public void addVariable (String id, HierarchicalNode node, int index, boolean isConcentration) {
-		if (header.length() == 0) {
-			header.append("(\"" + id + "\"");
-		} else {
-			header.append(",\"" + id + "\"");
-		}
+  @Override
+  public void init(String filename) throws IOException {
+    if (!isSet && header.length() > 0) {
+      header.append(")");
+      isSet = true;
+    }
+    if (isSet) {
+      writer = new FileWriter(filename);
+      bufferedWriter = new BufferedWriter(writer);
+      bufferedWriter.write('(');
+      bufferedWriter.write(header.toString());
+      bufferedWriter.flush();
+    }
+  }
 
-		addNode(node, index, isConcentration);
-	}
-
-	@Override
-	public void init (String filename) throws IOException {
-		if (!isSet && header.length() > 0) {
-			header.append(")");
-			isSet = true;
-		}
-		if (isSet) {
-			writer = new FileWriter(filename);
-			bufferedWriter = new BufferedWriter(writer);
-			bufferedWriter.write('(');
-			bufferedWriter.write(header.toString());
-			bufferedWriter.flush();
-		}
-	}
-
-	@Override
-	public void close () throws IOException {
-		bufferedWriter.write(")");
-		bufferedWriter.close();
-	}
+  @Override
+  public void close() throws IOException {
+    bufferedWriter.write(")");
+    bufferedWriter.close();
+  }
 
 }
