ce8d81b8cee93affb400727b650422871c69d7de
Pedro Fontanarrosa <pfontanarrosa@gmail.com>
2021-01-19 19:37:38 -0300

    Fixed typo
    Changed beta for TauON and TauOFF

--- conversion/src/main/java/edu/utah/ece/async/ibiosim/conversion/FlowModel.java
+++ conversion/src/main/java/edu/utah/ece/async/ibiosim/conversion/FlowModel.java
@@ -872,12 +872,12 @@ public class FlowModel {
 						if (Annot.get(i).getQName().toString().equals(new String("{http://cellocad.org/Terms/cello#}beta"))) {
 							beta = Annot.get(i).getStringValue();
 						}
-						if (Annot.get(i).getQName().toString().equals(new String("{http://cellocad.org/Terms/cello#}tauon"))) {
-							beta = Annot.get(i).getStringValue();
+						if (Annot.get(i).getQName().toString().equals(new String("{http://cellocad.org/Terms/cello#}tau_on"))) {
+							TauON = Annot.get(i).getStringValue();
 						}
 						//TODO PEDRO CHECK IF ANNOTATION IS REALLY TAUON OR HOW.
-						if (Annot.get(i).getQName().toString().equals(new String("{http://cellocad.org/Terms/cello#}tauoff"))) {
-							beta = Annot.get(i).getStringValue();
+						if (Annot.get(i).getQName().toString().equals(new String("{http://cellocad.org/Terms/cello#}tau_off"))) {
+							TauOFF = Annot.get(i).getStringValue();
 						}
 					}
 			}
